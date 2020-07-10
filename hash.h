824b4fcca306177ae690ae6789202a0d40cd7684
unknown <bruno_spoletini@hotmail.com>
2020-07-10 16:43:44 -0300

    Archivos creados

---
+++ hash.h
@@ -0,0 +1,30 @@
+#ifndef __HASH_H__
+#define __HASH_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/*/
+typedef struct _intervalo{
+  double inicio;
+  double final;
+}intervalo;
+
+
+typedef struct _iNodo{
+  intervalo *intervalo;
+  double maximo;
+  int altura;
+  struct _iNodo *izq;
+  struct _iNodo *der;
+} iNodo;
+
+typedef iNodo *iTree;
+
+typedef void (*funcionVisitante) (iTree nodo);
+/* /
+
+
+
+#endif /* __HASH_H__ */
